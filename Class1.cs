5ce0a3f5d58d08a68b167ba2b6698eadf9388902
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-15 12:03:15 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+public interface IData
+{
+    public string GetData();
+}
+public interface IName:IData
+{
+    public string GetName();
+}
+public class Person : IName 
+{
+    public string GetData()
+    {
+        return "Data";
+    }
+    public string GetName()
+    {
+        return "Person";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Linq;
+
+namespace Class_0515_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IName n1 = new Person();
+            Console.WriteLine(n1.GetData());
+            Console.WriteLine(n1.GetName());
+        }
+    }
+}
